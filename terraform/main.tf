25e5eeaca768b15506282c63bbb93e117f25b36f
maurosluciosprojetos <maurosluciosprojetos@gmail.com>
2025-03-04 16:36:07 -0300

    arquivo principal terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,20 @@
-terraform{
-
+terraform {
+  required_version = ">=1.11.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" # 5.0.0 ate 5.0.n sendo n a última versão 5 exemplo: 5.0.15
+    }
+  }
 }
 
 provider "aws" {
   region = "us-west-1"
-  
+  default_tags {
+    tags = {
+      owner      = "mauroslucioss"
+      managed-by = "terraform"
+    }
+  }
 }
 
-resource "aws_instance" "vm1" {
-  
-}
-
-data "aws_ami" "image1" {
-  
-}
-
-module "network" {
-
-}
-  
-variable "vm_name" {
-
-}
-
-output "vm1_ip" {
-  value = var.vm_name
-}
-
-locals {
-  
-}
